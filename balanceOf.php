ec4f684a54df7bd9d993cd65188df17b62eb1966
Alexander Strakh <cromlehg@gmail.com>
2022-06-15 12:10:13 +0300

    Replace ERC20 with ERC721

--- balanceOf.php
+++ balanceOf.php
@@ -4,16 +4,16 @@ require 'vendor/autoload.php';
 
 use Web3\Contract;
 
-$contractAddress = '0x085D15DB9c7Cd3Df188422f88Ec41ec573D691b9';
+$contractAddress = '0x363189488bCD7B928DE7f954A131637Fac0fe4b0';
 
 $abi = '[{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]';
 
-$contract = new Contract('https://bsc-dataseed.binance.org', $abi);
+$contract = new Contract('https://data-seed-prebsc-1-s1.binance.org:8545', $abi);
 
 $contractInstance = $contract->at($contractAddress);
 
 // Address of holder who wants to know token balance
-$holder = '0x085D15DB9c7Cd3Df188422f88Ec41ec573D691b9';
+$holder = '0x0edf5620d75eef555f927cfd28679494733f2ccd';
 
 $contractInstance->call('balanceOf', $holder, function ($err, $response) {
     if ($err !== null) {
